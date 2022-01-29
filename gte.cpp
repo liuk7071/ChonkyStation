82bc86202625c1af68f2f8f5e55d9fb31bcf388a
liuk7071 <lewk707@gmail.com>
2022-01-29 19:51:27 +0100

    more GTE fixes

--- gte.cpp
+++ gte.cpp
@@ -15,19 +15,58 @@ void gte::execute(uint32_t instr, uint32_t* gpr) {
 		}
 		break;
 	}
-	case RTPS: commandRTPS(); break;
-	case NCLIP: commandNCLIP(); break;
-	case NCDS: commandNCDS(); break;
-	case AVSZ3: commandAVSZ3(); break;
-	case AVSZ4: commandAVSZ4(); break;
-	case RTPT: commandRTPT(); break;
+	case RTPS: cop2c[31] = 0; commandRTPS(); break;
+	case NCLIP: cop2c[31] = 0; commandNCLIP(); break;
+	case NCDS: cop2c[31] = 0; commandNCDS(); break;
+	case AVSZ3: cop2c[31] = 0; commandAVSZ3(); break;
+	case AVSZ4: cop2c[31] = 0; commandAVSZ4(); break;
+	case RTPT: cop2c[31] = 0; commandRTPT(); break;
 	default:
 		printf("Unimplemented GTE instruction: 0x%x\n", instr);
 		exit(1);
 	}
 }
 
+
 // Helpers
+
+uint32_t gte::readCop2d(uint32_t reg) {
+	switch (reg) {
+	case 12:
+	case 13:
+	case 14:
+	case 22: {
+		return cop2d[reg];
+	}
+	case 8: {
+		return (uint32_t)(int16_t)(cop2d[reg]);
+	}
+	default:
+		printf("Unhandled cop2d read %d\n", reg);
+		exit(1);
+	}
+}
+void gte::writeCop2d(uint32_t reg, uint32_t value) {
+	switch (reg) {
+	case 0: 
+	case 2:
+	case 4:
+	case 6: {
+		cop2d[reg] = value;
+		break;
+	}
+	case 1:
+	case 3:
+	case 5: {
+		cop2d[reg] = (uint32_t)(int16_t)(value);
+		break;
+	}
+	default:
+		printf("Unhandled cop2d write %d\n", reg);
+		exit(1);
+	}
+}
+
 // Push a Z value to the Z-coordinate FIFO
 void gte::pushZ(uint16_t value) {
 	SZ0 = SZ1;
@@ -53,19 +92,27 @@ void gte::setIRFromMAC() {
 // Commands
 void gte::moveMFC2(uint32_t* gpr) {
 	switch ((instruction >> 11) & 0x1f) {
-	case 24: {
+	case 7: {
+		//printf("cop2r%d (0x%x) -> r%d\n", (instruction >> 11) & 0x1f, cop2d[(instruction >> 11) & 0x1f], (instruction >> 16) & 0x1f);
 		gpr[(instruction >> 16) & 0x1f] = cop2d[(instruction >> 11) & 0x1f];
 		break;
 	}
+	case 24: {
+		//printf("cop2r%d (0x%x) -> r%d\n", (instruction >> 11) & 0x1f, cop2d[(instruction >> 11) & 0x1f], (instruction >> 16) & 0x1f);
+		gpr[(instruction >> 16) & 0x1f] = cop2d[(instruction >> 11) & 0x1f];
+		//gpr[(instruction >> 16) & 0x1f] = 200;
+		break;
+	}
 	default:
-		printf("Unimplemented MFC2 destination: %d\n", (instruction >> 11) & 0x1f);
+		//printf("Unimplemented MFC2 destination: %d\n", (instruction >> 11) & 0x1f);
 		exit(1);
 	}
 }
 void gte::moveMTC2(uint32_t* gpr) {
 	switch ((instruction >> 11) & 0x1f) {
 	case 8: {
-		cop2d[(instruction >> 11) & 0x1f] = gpr[(instruction >> 16) & 0x1f];
+		//printf("0x%x -> cop2r%d\n", (uint32_t)(int16_t)gpr[(instruction >> 16) & 0x1f], (instruction >> 11) & 0x1f);
+		cop2d[(instruction >> 11) & 0x1f] = (uint32_t)(int16_t)(gpr[(instruction >> 16) & 0x1f]);
 		break;
 	}
 	default:
@@ -74,6 +121,7 @@ void gte::moveMTC2(uint32_t* gpr) {
 	}
 }
 void gte::moveCFC2(uint32_t* gpr) {
+	//printf("cnt%d (0x%x) -> r%d\n", (instruction >> 11) & 0x1f, cop2c[(instruction >> 11) & 0x1f], (instruction >> 16) & 0x1f);
 	gpr[(instruction >> 16) & 0x1f] = cop2c[(instruction >> 11) & 0x1f];
 }
 void gte::moveCTC2(uint32_t* gpr) {
@@ -86,7 +134,7 @@ void gte::moveCTC2(uint32_t* gpr) {
 	case 27:
 	case 29:
 	case 30: {
-		//cop2c[(instruction >> 11) & 0x1f] = (gpr[(instruction >> 16) & 0x1f] & 0xffff);
+		//printf("0x%x -> cnt%d\n", (uint32_t)(int16_t)gpr[(instruction >> 16) & 0x1f], (instruction >> 11) & 0x1f);
 		cop2c[(instruction >> 11) & 0x1f] = (uint32_t)(int16_t)(gpr[(instruction >> 16) & 0x1f]);
 		break;
 	}
@@ -127,22 +175,42 @@ void gte::moveCTC2(uint32_t* gpr) {
 
 void gte::commandRTPS() {
 	const int shift = sf(instruction) * 12;
-	MAC1 = int32_t((TRX * 0x1000) + (RT11 * VX0) + (RT12 * VY0) + (RT13 * VZ0)) >> shift;
-	MAC2 = int32_t((TRY * 0x1000) + (RT21 * VX0) + (RT22 * VY0) + (RT23 * VZ0)) >> shift;
-	MAC3 = int32_t((TRZ * 0x1000) + (RT31 * VX0) + (RT32 * VY0) + (RT33 * VZ0)) >> shift;
+	MAC1 = int64_t(((int64_t)(int32_t)TRX * 0x1000) + ((int16_t)RT11 * (int16_t)VX0) + ((int16_t)RT12 * (int16_t)VY0) + ((int16_t)RT13 * (int16_t)VZ0)) >> shift;
+	MAC2 = int64_t(((int64_t)(int32_t)TRY * 0x1000) + ((int16_t)RT21 * (int16_t)VX0) + ((int16_t)RT22 * (int16_t)VY0) + ((int16_t)RT23 * (int16_t)VZ0)) >> shift;
+	MAC3 = int64_t(((int64_t)(int32_t)TRZ * 0x1000) + ((int16_t)RT31 * (int16_t)VX0) + ((int16_t)RT32 * (int16_t)VY0) + ((int16_t)RT33 * (int16_t)VZ0)) >> shift;
 	setIRFromMAC();
-	const auto newZ = int32_t(MAC3) >> ((1 - sf(instruction)) * 12);
+	auto newZ = int32_t(MAC3) >> ((1 - sf(instruction)) * 12);
 	pushZ(newZ);
 
 	SXY0 = SXY1;
 	SXY1 = SXY2;
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR1) + OFX; SETSX2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR2) + OFY; SETSY2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * DQA) + DQB; IR0 = (MAC0 / 0x1000);
+	//uint32_t _proj_factor = (((((uint32_t)(H) * 0x20000) / (uint32_t)(SZ3)) + 1) / 2);
+	int32_t _proj_factor = gte_divide(H, SZ3);
+	int64_t proj_factor = (int64_t)(_proj_factor);
+	int64_t _x = (int64_t)(int16_t)(IR1);
+	int64_t _y = (int64_t)(int16_t)(IR2);
+	int64_t x = ((_x * proj_factor) + (int64_t)(int32_t)(OFX));
+	int64_t y = ((_y * proj_factor) + (int64_t)(int32_t)(OFY));
+	SETSX2((x >> 16));
+	SETSY2((y >> 16));
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR1)) + OFX; SETSX2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR2)) + OFY; SETSY2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * DQA) + DQB; IR0 = 
+	int64_t depth = ((int64_t)DQB + ((int64_t)DQA * proj_factor));
+	MAC0 = (int32_t)(depth);
+	depth >>= 12;
+	IR0 = (int16_t)(depth);
 }
 
 void gte::commandNCLIP() {
-	MAC0 = (SX0 * SY1) + (SX1 * SY2) + (SX2 * SY0) - (SX0 * SY2) - (SX1 * SY0) - (SX2 * SY1);
+	MAC0 = ((int32_t)(SX0) * (int32_t)(SY1)) + ((int32_t)(SX1) * (int32_t)(SY2)) + ((int32_t)(SX2) * (int32_t)(SY0)) - ((int32_t)(SX0) * (int32_t)(SY2)) - ((int32_t)(SX1) * (int32_t)(SY0)) - ((int32_t)(SX2) * (int32_t)(SY1));
+	//printf("sx0: %d, sy0: %d sx1: %d, sy1: %d sx2: %d, sy2: %d\n", SX0, SY0, SX1, SY1, SX2, SY2);
+	//auto a = (int32_t)(SX0) * ((int32_t)(SY1) - (int32_t)(SY2));
+	//auto b = (int32_t)(SX1) * ((int32_t)(SY2) - (int32_t)(SY0));
+	//auto c = (int32_t)(SX2) * ((int32_t)(SY0) - (int32_t)(SY1));
+	//MAC0 = (int32_t)(a + b + c);
+	////MAC0 = 0x70;
+	//printf("%d\n", MAC0);
 }
 
 void gte::commandNCDS() {
@@ -155,12 +223,12 @@ void gte::commandNCDS() {
 	MAC2 = int32_t((GBK * 0x1000) + ((LG1 * IR1) + (LG2 * IR2) + (LG3 * IR3))) >> shift;
 	MAC1 = int32_t((BBK * 0x1000) + ((LB1 * IR1) + (LB2 * IR2) + (LB3 * IR3))) >> shift;
 	setIRFromMAC();
-	MAC1 = (R * IR1) << 4;
-	MAC2 = (G * IR2) << 4;
-	MAC3 = (B * IR3) << 4;
-	MAC1 = MAC1 + ((RFC - MAC1) * IR0);
-	MAC2 = MAC2 + ((GFC - MAC2) * IR0);
-	MAC3 = MAC3 + ((BFC - MAC3) * IR0);
+	MAC1 = (R * ((int16_t)IR1)) << 4;
+	MAC2 = (G * ((int16_t)IR2)) << 4;
+	MAC3 = (B * ((int16_t)IR3)) << 4;
+	MAC1 = MAC1 + ((RFC - MAC1) * ((int16_t)IR0));
+	MAC2 = MAC2 + ((GFC - MAC2) * ((int16_t)IR0));
+	MAC3 = MAC3 + ((BFC - MAC3) * ((int16_t)IR0));
 	MAC1 = int32_t(MAC1) >> shift;
 	MAC2 = int32_t(MAC2) >> shift;
 	MAC3 = int32_t(MAC3) >> shift;
@@ -181,37 +249,83 @@ void gte::commandAVSZ4() {
 
 void gte::commandRTPT() {
 	const int shift = sf(instruction) * 12;
-
-	MAC1 = int32_t((TRX * 0x1000) + (RT11 * VX0) + (RT12 * VY0) + (RT13 * VZ0)) >> shift;
-	MAC2 = int32_t((TRY * 0x1000) + (RT21 * VX0) + (RT22 * VY0) + (RT23 * VZ0)) >> shift;
-	MAC3 = int32_t((TRZ * 0x1000) + (RT31 * VX0) + (RT32 * VY0) + (RT33 * VZ0)) >> shift;
+	MAC1 = int64_t(((int64_t)(int32_t)(TRX) * 0x1000) + ((int16_t)RT11 * (int16_t)VX0) + ((int16_t)RT12 * (int16_t)VY0) + ((int16_t)RT13 * (int16_t)VZ0)) >> shift;
+	MAC2 = int64_t(((int64_t)(int32_t)(TRY) * 0x1000) + ((int16_t)RT21 * (int16_t)VX0) + ((int16_t)RT22 * (int16_t)VY0) + ((int16_t)RT23 * (int16_t)VZ0)) >> shift;
+	MAC3 = int64_t(((int64_t)(int32_t)(TRZ) * 0x1000) + ((int16_t)RT31 * (int16_t)VX0) + ((int16_t)RT32 * (int16_t)VY0) + ((int16_t)RT33 * (int16_t)VZ0)) >> shift;
 	setIRFromMAC();
-	pushZ(int32_t(MAC3) >> ((1 - sf(instruction)) * 12));
+	auto newZ = int32_t(MAC3) >> ((1 - sf(instruction)) * 12);
+	pushZ(newZ);
+
 	SXY0 = SXY1;
 	SXY1 = SXY2;
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR1) + OFX; SETSX2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR2) + OFY; SETSY2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * DQA) + DQB; IR0 = (MAC0 / 0x1000);
+	//uint32_t _proj_factor = (((((uint32_t)(H) * 0x20000) / (uint32_t)(SZ3)) + 1) / 2);
+	int32_t _proj_factor = gte_divide(H, SZ3);
+	int64_t proj_factor = (int64_t)(_proj_factor);
+	int64_t _x = (int64_t)(int16_t)(IR1);
+	int64_t _y = (int64_t)(int16_t)(IR2);
+	int64_t x = ((_x * proj_factor) + (int64_t)(int32_t)(OFX));
+	int64_t y = ((_y * proj_factor) + (int64_t)(int32_t)(OFY));
+	SETSX2((uint32_t)(x >> 16));
+	SETSY2((uint32_t)(y >> 16));
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR1)) + OFX; SETSX2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR2)) + OFY; SETSY2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * DQA) + DQB; IR0 = 
+	int64_t depth = ((int64_t)DQB + ((int64_t)DQA * proj_factor));
+	MAC0 = (int32_t)(depth);
+	depth >>= 12;
+	IR0 = (int16_t)(depth);
 
-	MAC1 = int32_t((TRX * 0x1000) + (RT11 * VX0) + (RT12 * VY0) + (RT13 * VZ0)) >> shift;
-	MAC2 = int32_t((TRY * 0x1000) + (RT21 * VX0) + (RT22 * VY0) + (RT23 * VZ0)) >> shift;
-	MAC3 = int32_t((TRZ * 0x1000) + (RT31 * VX0) + (RT32 * VY0) + (RT33 * VZ0)) >> shift;
+
+	MAC1 = int64_t(((int64_t)(int32_t)(TRX) * 0x1000) + ((int16_t)RT11 * (int16_t)VX1) + ((int16_t)RT12 * (int16_t)VY1) + ((int16_t)RT13 * (int16_t)VZ1)) >> shift;
+	MAC2 = int64_t(((int64_t)(int32_t)(TRY) * 0x1000) + ((int16_t)RT21 * (int16_t)VX1) + ((int16_t)RT22 * (int16_t)VY1) + ((int16_t)RT23 * (int16_t)VZ1)) >> shift;
+	MAC3 = int64_t(((int64_t)(int32_t)(TRZ) * 0x1000) + ((int16_t)RT31 * (int16_t)VX1) + ((int16_t)RT32 * (int16_t)VY1) + ((int16_t)RT33 * (int16_t)VZ1)) >> shift;
 	setIRFromMAC();
-	pushZ(int32_t(MAC3) >> ((1 - sf(instruction)) * 12));
+	newZ = int32_t(MAC3) >> ((1 - sf(instruction)) * 12);
+	pushZ(newZ);
+
 	SXY0 = SXY1;
 	SXY1 = SXY2;
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR1) + OFX; SETSX2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR2) + OFY; SETSY2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * DQA) + DQB; IR0 = (MAC0 / 0x1000);
+	//_proj_factor = (((((uint32_t)(H) * 0x20000) / (uint32_t)(SZ3)) + 1) / 2);
+	_proj_factor = gte_divide(H, SZ3);
+	proj_factor = (int64_t)(_proj_factor);
+	_x = (int64_t)(int16_t)(IR1);
+	_y = (int64_t)(int16_t)(IR2);
+	x = ((_x * proj_factor) + (int64_t)(int32_t)(OFX));
+	y = ((_y * proj_factor) + (int64_t)(int32_t)(OFY));
+	SETSX2((uint32_t)(x >> 16));
+	SETSY2((uint32_t)(y >> 16));
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR1)) + OFX; SETSX2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR2)) + OFY; SETSY2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * DQA) + DQB; IR0 = 
+	depth = ((int64_t)DQB + ((int64_t)DQA * proj_factor));
+	MAC0 = (int32_t)(depth);
+	depth >>= 12;
+	IR0 = (int16_t)(depth);
 
-	MAC1 = int32_t((TRX * 0x1000) + (RT11 * VX0) + (RT12 * VY0) + (RT13 * VZ0)) >> shift;
-	MAC2 = int32_t((TRY * 0x1000) + (RT21 * VX0) + (RT22 * VY0) + (RT23 * VZ0)) >> shift;
-	MAC3 = int32_t((TRZ * 0x1000) + (RT31 * VX0) + (RT32 * VY0) + (RT33 * VZ0)) >> shift;
+	MAC1 = int64_t(((int64_t)(int32_t)(TRX) * 0x1000) + ((int16_t)RT11 * (int16_t)VX2) + ((int16_t)RT12 * (int16_t)VY2) + ((int16_t)RT13 * (int16_t)VZ2)) >> shift;
+	MAC2 = int64_t(((int64_t)(int32_t)(TRY) * 0x1000) + ((int16_t)RT21 * (int16_t)VX2) + ((int16_t)RT22 * (int16_t)VY2) + ((int16_t)RT23 * (int16_t)VZ2)) >> shift;
+	MAC3 = int64_t(((int64_t)(int32_t)(TRZ) * 0x1000) + ((int16_t)RT31 * (int16_t)VX2) + ((int16_t)RT32 * (int16_t)VY2) + ((int16_t)RT33 * (int16_t)VZ2)) >> shift;
 	setIRFromMAC();
-	pushZ(int32_t(MAC3) >> ((1 - sf(instruction)) * 12));
+	newZ = int32_t(MAC3) >> ((1 - sf(instruction)) * 12);
+	pushZ(newZ);
+
 	SXY0 = SXY1;
 	SXY1 = SXY2;
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR1) + OFX; SETSX2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * IR2) + OFY; SETSY2(MAC0 / 0x10000);
-	MAC0 = (((((H * 0x20000) / SZ3) + 1) / 2) * DQA) + DQB; IR0 = MAC0 / 0x1000;
+	//_proj_factor = (((((uint32_t)(H) * 0x20000) / (uint32_t)(SZ3)) + 1) / 2);
+	_proj_factor = gte_divide(H, SZ3);
+	proj_factor = (int64_t)(_proj_factor);
+	_x = (int64_t)(int16_t)(IR1);
+	_y = (int64_t)(int16_t)(IR2);
+	x = ((_x * proj_factor) + (int64_t)(int32_t)(OFX));
+	y = ((_y * proj_factor) + (int64_t)(int32_t)(OFY));
+	SETSX2((uint32_t)(x >> 16));
+	SETSY2((uint32_t)(y >> 16));
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR1)) + OFX; SETSX2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((int64_t)(((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * (int64_t)(int16_t)(IR2)) + OFY; SETSY2(((int32_t)(MAC0)) / 0x10000);
+	//MAC0 = ((((((uint16_t)(H) * 0x20000) / (uint16_t)(SZ3)) + 1) / 2) * DQA) + DQB; IR0 = 
+	depth = ((int64_t)DQB + ((int64_t)DQA * proj_factor));
+	MAC0 = (int32_t)(depth);
+	depth >>= 12;
+	IR0 = (int16_t)(depth);
+	printf("%x: %d, %d\n", SXY2, SX2, SY2);
 }
