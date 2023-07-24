03eb3e9767d64ebd2cdd10761ce6e7ac0ccd3ce3
liuk7071 <lewk707@gmail.com>
2023-07-24 15:48:06 +0200

    [CPU] Make the switchBackend function actually switch backends

--- src/cpu/cpu.cpp
+++ src/cpu/cpu.cpp
@@ -6,5 +6,10 @@ void Cpu::step() {
 }
 
 void Cpu::switchBackend(Backend backend) {
+    switch (backend) {
+    case Backend::Interpreter:    stepFunc = &interpreter.step; break;
+    case Backend::OldInterpreter: stepFunc = &oldInterpreter.step; break;
+    default: Helpers::panic("Unsupported backend\n");   // Should never be triggered
+    }
     stepFunc = &oldInterpreter.step;
 }
