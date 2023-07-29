ac3e17b031b601dfb8d7e15d1e26a4ca831ad325
liuk7071 <lewk707@gmail.com>
2023-07-29 17:33:18 +0200

    Attempt 3 at fixing MacOS CI

--- .github/macos-bundle.sh
+++ .github/macos-bundle.sh
@@ -4,18 +4,18 @@ PATH="$PATH:/usr/libexec"
 
 
 # Construct the app iconset.
-mkdir ChonkyStation.iconset
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 16x16 ChonkyStation.iconset/icon_16x16.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 32x32 ChonkyStation.iconset/icon_16x16@2x.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 32x32 ChonkyStation.iconset/icon_32x32.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 64x64 ChonkyStation.iconset/icon_32x32@2x.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 128x128 ChonkyStation.iconset/icon_128x128.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 256x256 ChonkyStation.iconset/icon_128x128@2x.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 256x256 ChonkyStation.iconset/icon_256x256.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 512x512 ChonkyStation.iconset/icon_256x256@2x.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 512x512 ChonkyStation.iconset/icon_512x512.png
-convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 1024x1024 ChonkyStation.iconset/icon_512x512@2x.png
-iconutil --convert icns ChonkyStation.iconset
+mkdir chonkystation.iconset
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 16x16 chonkystation.iconset/icon_16x16.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 32x32 chonkystation.iconset/icon_16x16@2x.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 32x32 chonkystation.iconset/icon_32x32.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 64x64 chonkystation.iconset/icon_32x32@2x.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 128x128 chonkystation.iconset/icon_128x128.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 256x256 chonkystation.iconset/icon_128x128@2x.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 256x256 chonkystation.iconset/icon_256x256.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 512x512 chonkystation.iconset/icon_256x256@2x.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 72 -resize 512x512 chonkystation.iconset/icon_512x512.png
+convert docs/img/icon.ico -alpha on -background none -units PixelsPerInch -density 144 -resize 1024x1024 chonkystation.iconset/icon_512x512@2x.png
+iconutil --convert icns chonkystation.iconset
 
 # Set up the .app directory
 mkdir -p ChonkyStation.app/Contents/MacOS/Libraries
@@ -27,7 +27,7 @@ cp ./build/ChonkyStation ChonkyStation.app/Contents/MacOS/ChonkyStation
 chmod a+x ChonkyStation.app/Contents/Macos/ChonkyStation
 
 # Copy icons into App
-cp ChonkyStation.icns ChonkyStation.app/Contents/Resources/AppIcon.icns
+cp chonkystation.icns ChonkyStation.app/Contents/Resources/AppIcon.icns
 
 # Fix up Plist stuff
 PlistBuddy ChonkyStation.app/Contents/Info.plist -c "add CFBundleDisplayName string ChonkyStation"
